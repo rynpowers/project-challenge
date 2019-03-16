64f1b61f71fa286668a35b31541c8ba6836a8443
Ryan <rynpowers@gmail.com>
2019-03-16 13:45:19 -0400

    added likes to dog index page

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,9 @@ class Dog < ApplicationRecord
   has_many_attached :images
   belongs_to :user
 
+  has_many :likes
+  has_many :liking_users, :through => :likes, :source => :user
+
   validate :image_type
 
   def image_type
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_161449) do
+ActiveRecord::Schema.define(version: 2019_03_16_170215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,15 @@ ActiveRecord::Schema.define(version: 2019_03_14_161449) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "dog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_likes_on_dog_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
